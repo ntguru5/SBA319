0e77668b5bd4374db875803201b5c6bf0194d3ca
Ken Yeh <yehken@gmail.com>
2024-11-06 23:21:40 -0800

    working on GET routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import dogRoutes from './routes/dogs.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,12 @@ app.use(express.json()); // for parsing application/json
 // connect to database
 await mongoose.connect(process.env.ATLAS_URI);
 console.log("Connected to database");
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API.");
+});
+
+app.use('/dogs', dogRoutes);
 
 // start server
 app.listen(PORT, () => {
